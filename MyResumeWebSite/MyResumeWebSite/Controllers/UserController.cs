036154b53e48a6aa824397e7baf2b870095f1b7b
Erman Yükselgün <68476878+ermanyukselgun@users.noreply.github.com>
2021-03-17 03:22:55 +0300

    Entity added
    Entity Framework has been added and the admin panel has been prepared.

---
+++ MyResumeWebSite/MyResumeWebSite/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using MyResumeWebSite.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyResumeWebSite.Controllers
+{
+    public class UserController : Controller
+    {
+        MyResumeEntities _dbContext = new MyResumeEntities();     //connection
+        // GET: User
+        public ActionResult Index()
+        {
+            var model = _dbContext.User.ToList();
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(User user)
+        {
+            var model = _dbContext.User.Add(user);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
