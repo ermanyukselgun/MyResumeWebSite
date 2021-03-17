1340a858b19b7d187b53c7a5877d82282120fdf0
Erman Yükselgün <68476878+ermanyukselgun@users.noreply.github.com>
2021-03-17 12:27:28 +0300

    User information table has been prepared

--- MyResumeWebSite/Controllers/UserController.cs
+++ MyResumeWebSite/Controllers/UserController.cs
@@ -21,15 +21,57 @@ namespace MyResumeWebSite.Controllers
         [HttpGet]
         public ActionResult AddUser()
         {
+            
             return View();
         }
 
         [HttpPost]
+        
         public ActionResult AddUser(User user)
         {
-            var model = _dbContext.User.Add(user);
+            
+            if(user.Id == 0)
+            {
+                _dbContext.User.Add(user);
+            }
+            else
+            {
+                //update //Classic Method
+                var update = _dbContext.User.Find(user.Id);
+                if(update == null)
+                {
+                    return HttpNotFound();
+                }
+                update.Name = user.Name;
+                update.Surname = user.Surname;
+                update.Phone = user.Phone;
+                update.Adress = user.Adress;
+            }
+
             _dbContext.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index","User");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var model = _dbContext.User.Find(id);  //get id
+
+
+            return View("AddUser", model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var delete = _dbContext.User.Find(id);
+
+            if (delete == null)
+            {
+                return HttpNotFound();
+            }
+            _dbContext.User.Remove(delete);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "User");
         }
     }
 }
