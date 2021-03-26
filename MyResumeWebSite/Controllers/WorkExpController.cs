1542b8c8f34d5b6930d2d0dd83474b03992a6435
Erman Yükselgün <68476878+ermanyukselgun@users.noreply.github.com>
2021-03-26 16:17:48 +0300

    Added WorkExp. table

---
+++ MyResumeWebSite/Controllers/WorkExpController.cs
@@ -0,0 +1,72 @@
+﻿using MyResumeWebSite.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyResumeWebSite.Controllers
+{
+    public class WorkExpController : Controller
+    {
+        MyResumeEntities1 _dbContext = new MyResumeEntities1();     //connection
+
+        // GET: WorkExp
+        public ActionResult Index()
+        {
+            var Experiens = _dbContext.WorkExperiance.ToList();
+            return View(Experiens);
+        }
+
+        [HttpGet]
+        public ActionResult AddExperiens()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddExperiens(WorkExperiance workExperiance)
+        {
+            if (workExperiance.Id == 0)
+            {
+                _dbContext.WorkExperiance.Add(workExperiance);
+            }
+            else
+            {
+                //Update
+                _dbContext.Entry(workExperiance).State = System.Data.Entity.EntityState.Modified;
+            }
+
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "WorkExp");
+        }
+
+        //Update
+
+        public ActionResult UpdateExperiens(int id)
+        {
+            var update = _dbContext.WorkExperiance.Find(id);
+            if (update == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("AddExperiens", update);
+        }
+
+        public ActionResult DeleteExperiens(int id)
+        {
+            var del = _dbContext.WorkExperiance.Find(id);
+
+            if (del == null)
+            {
+                return HttpNotFound();
+            }
+
+            _dbContext.WorkExperiance.Remove(del);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "WorkExp");
+        }
+    }
+}
