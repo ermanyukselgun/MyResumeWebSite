704e15346993bfb5f316289c2c274e71cb2dbec3
Erman Yükselgün <68476878+ermanyukselgun@users.noreply.github.com>
2021-04-12 18:47:50 +0300

    Add homepage / personelinformation

--- MyResumeWebSite/Controllers/HomeController.cs
+++ MyResumeWebSite/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyResumeWebSite.Models;
+using MyResumeWebSite.Models.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,24 @@ namespace MyResumeWebSite.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        MyResumeEntities1 _dbContext = new MyResumeEntities1();  //Connection
+
+        
         public ActionResult Index()
         {
-            return View();
+            HomeModelView homeModelView = new HomeModelView();
+            homeModelView.users = _dbContext.User.ToList();
+            homeModelView.courses = _dbContext.Courses.ToList();
+            homeModelView.D_Management = _dbContext.DatabaseManagement.ToList();
+            homeModelView.educations = _dbContext.Education.ToList();
+            homeModelView.G_Technology = _dbContext.GameTechnology.ToList();
+            homeModelView.P_Language = _dbContext.ProgramingLanguage.ToList();
+            homeModelView.references = _dbContext.Reference.ToList();
+            homeModelView.socialMediaTables = _dbContext.SocialMediaTable.ToList();
+            homeModelView.W_Technology = _dbContext.WebTechnology.ToList();
+            homeModelView.workExperiances = _dbContext.WorkExperiance.ToList();
+
+            return View(homeModelView);
         }
     }
 }
---
+++ MyResumeWebSite/Models/HomeModelView.cs
@@ -0,0 +1,23 @@
+﻿using MyResumeWebSite.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyResumeWebSite.Models
+{
+    public class HomeModelView
+    {
+        public IEnumerable<User> users { get; set; }
+        public IEnumerable<Courses> courses { get; set; }
+        public IEnumerable<Education> educations { get; set; }
+        public IEnumerable<SocialMediaTable> socialMediaTables { get; set; }
+        public IEnumerable<Reference> references { get; set; }
+        public IEnumerable<ProgramingLanguage> P_Language { get; set; }
+        public IEnumerable<DatabaseManagement> D_Management { get; set; }
+        public IEnumerable<WebTechnology> W_Technology { get; set; }
+        public IEnumerable<GameTechnology> G_Technology { get; set; }
+        public IEnumerable<WorkExperiance> workExperiances { get; set; }
+
+    }
+}
