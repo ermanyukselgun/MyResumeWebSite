73f47ef9200258722decff04101e750bfee9632c
Erman Yükselgün <68476878+ermanyukselgun@users.noreply.github.com>
2021-04-20 13:52:48 +0300

    added project controller

--- MyResumeWebSite/Controllers/ProjectsController.cs
+++ MyResumeWebSite/Controllers/ProjectsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -48,6 +49,56 @@ namespace MyResumeWebSite.Controllers
             }
             
             return View(projects);
+        }
+
+        public ActionResult Update(int id)
+        {
+            var update = _dbContext.MyProjects.Find(id);
+            if (update == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(update);
+        }
+        [HttpPost]
+        public ActionResult Update(MyProject myProject, HttpPostedFileBase picture)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = _dbContext.MyProjects.Find(myProject.Id);
+                if (model != null && picture != null && picture.ContentLength > 0)
+                {
+                    FileInfo fileInfo = new FileInfo(picture.FileName);
+                    string yeni = Guid.NewGuid().ToString() + fileInfo.Extension;//12345.jpeg
+                    picture.SaveAs(Server.MapPath("~/Assets/img/" + yeni));
+                    myProject.Picture = yeni;
+
+                    model.projectName = myProject.projectName;
+                    model.projectContent = myProject.projectContent;
+                    model.projectLanguage = myProject.projectLanguage;
+                    model.Picture = myProject.Picture;
+                    _dbContext.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }
+            return View(myProject);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var del = _dbContext.MyProjects.Find(id);
+
+            if (del == null)
+            {
+                return HttpNotFound();
+            }
+
+            _dbContext.MyProjects.Remove(del);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
